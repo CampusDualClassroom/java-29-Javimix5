34d40433f4f33e880243a9b0675b6e38b893be4b
Javimix5 <javiusa2018@gmail.com>
2025-05-26 12:28:20 +0200

    Exercise29 Resuelto

--- src/main/java/com/campusdual/classroom/Exercise29.java
+++ src/main/java/com/campusdual/classroom/Exercise29.java
@@ -2,6 +2,10 @@ package com.campusdual.classroom;
 
 public class Exercise29 {
     public static void main(String[] args) {
-
+        try {
+            throw new Exception("Exception throwed");
+        } catch (Exception e) {
+            System.out.println("Exception caught: " + e.getMessage());
+        }
     }
 }
